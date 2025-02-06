95cd9ef3b09fde4eb7dbfe6d1d76fbffb23843e3
siheon <rlatlgjs357@gmail.com>
2025-02-06 17:35:03 +0900

    포켓몬 디테일 페이지 구현

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,28 +1,42 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import Button from "../components/Button";
+import MOCK_DATA from "../Data/MOCK_DATA";
 
+// 포켓몬 디테일 페이지
 const Detail = () => {
   const navigate = useNavigate();
 
+  const param = useParams();
+
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    setPokemon(MOCK_DATA.find((item) => item.id === Number(param.id)));
+  }, [param.id]);
+
   return (
-    <Wrap>
-      <Character
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-        alt="character"
-      />
-      <Name>이상해씨</Name>
-      <Description>타입 : 풀, 독</Description>
-      <Description>
-        풀과 독 타입의 포켓몬으로, 귀여운 식물 모양을 하고 있습니다.
-      </Description>
-      <Button
-        label="뒤로 가기"
-        background="black"
-        handleClick={() => navigate("/dex")}
-      />
-    </Wrap>
+    <>
+      {pokemon && (
+        <Wrap>
+          <Character src={pokemon.img_url} alt="character" />
+          <Name>{pokemon.korean_name}</Name>
+          <Description>
+            타입 :{" "}
+            {pokemon.types.map((type, index) => {
+              return index < pokemon.types.length - 1 ? `${type}, ` : type;
+            })}
+          </Description>
+          <Description>{pokemon.description}</Description>
+          <Button
+            label="뒤로 가기"
+            background="black"
+            handleClick={() => navigate("/dex")}
+          />
+        </Wrap>
+      )}
+    </>
   );
 };
 
